68f87e3ea0ca6a85d4387c5e353cfb0a62287579
Riyarde12 <yarden.rihsoni03@gmail.com>
2022-03-30 22:34:04 +0300

    Merge branch 'main' of https://github.com/yosefzemertov/AirBYY-backend

--- api/order/order.controller.js
+++ api/order/order.controller.js
@@ -1,13 +1,13 @@
-const orderService = require('./order.service.js')
+const orderService = require('./order.service.js');
 const logger = require('../../service/logger.service');
 
 
 // get orders list for admin only
 
-async function getOrders(req,res) {
+async function getOrders(req, res) {
     try {
-        const orders = await orderService.query()
-        res.json(orders)
+        const orders = await orderService.query();
+        res.json(orders);
     } catch (err) {
         logger.error('Failed to get orders', err);
         res.status(500).send({ err: 'Failed to get orders' });
@@ -16,29 +16,26 @@ async function getOrders(req,res) {
 
 // single order
 
-async function getOrderById(req,res) {
-try {
-    const orderId = req.params.id
-    const order = orderService.getById(orderId)
-    
-    res.json(order)
-} catch (err) {
-    logger.error('Failed to get order', err);
-    res.status(500).send({ err: 'Failed to get order' });
-}
-}
-
-async function addOrder(req,res) {
+async function getOrderById(req, res) {
     try {
-        // const user = req.session.user
-        const orderToSave = req.body
-        console.log('addOrder line 35 :>>', orderToSave);
-        // orderToSave.userId = user._id 
-        console.log('addedOrder order controller line36 :>>' , orderToSave);
-        const addedOrder = await orderService.add(orderToSave)
+        const orderId = req.params.id;
+        const order = orderService.getById(orderId);
 
-          
-        res.send(addedOrder)
+        res.json(order);
+    } catch (err) {
+        logger.error('Failed to get order', err);
+        res.status(500).send({ err: 'Failed to get order' });
+    }
+}
+
+async function addOrder(req, res) {
+    try {
+        const orderToSave = req.body;
+        console.log('addOrder line 35 :>>', orderToSave);
+        console.log('addedOrder order controller line36 :>>', orderToSave);
+        const addedOrder = await orderService.add(orderToSave);
+        console.log('addOrder inline 39 (addedOrder) :>>', addedOrder.ops);
+        res.json(...addedOrder.ops);
     } catch (err) {
         logger.error('Failed to add order', err);
         // res.status(500).send({ err: 'Failed to add order' });
@@ -49,5 +46,5 @@ module.exports = {
     getOrders,
     getOrderById,
     addOrder
-}
+};
 
